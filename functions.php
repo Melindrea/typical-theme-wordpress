bbcf9ea2dafc70ddf02279f62724a45b0e03b3d3
Marie Hogebrandt <iam@mariehogebrandt.se>
2014-10-31 08:12:27 +0100

    Twitter mentions

--- functions.php
+++ functions.php
@@ -5,6 +5,14 @@
  * @package WordPress
  * @subpackage Typical
  */
+
+/** Replaces @mentions with twitter link, from http://bradsknutson.com/blog/automatically-replace-twitter-username-with-link-wordpress/ */
+function twitter_replace($content) {
+	$twitter_replace = preg_replace('/([^a-zA-Z0-9-_&])@([0-9a-zA-Z_]+)/',"$1<a href=\"http://twitter.com/$2\" target=\"_blank\" rel=\"nofollow\">@$2</a>",$content);
+	return $twitter_replace;
+}
+add_filter('the_content', 'twitter_replace');
+
 /** Tell WordPress to run typical_setup() when the 'after_setup_theme' hook is run. */
 
 add_action( 'after_setup_theme', 'typical_setup' );
